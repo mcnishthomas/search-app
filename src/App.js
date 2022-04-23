d28a422d8d13d18eac938f2354bc7ee93fb59aff
Thomas McNish <mcnishthomas@gmail.com>
2022-04-23 09:54:36 -0400

    added Results component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Container, Carousel } from 'react-bootstrap';
 import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Carousel>
-          <Container>
+          <container>
             <h1>Search App</h1>
             <p>This is a simple search app</p>
 
             <SearchForm />
-          </Container>
+          </container>
         </Carousel>
+        <Results />
       </div>
     );
   }
